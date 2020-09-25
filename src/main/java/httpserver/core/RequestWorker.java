d53730bf99eef1a5ef65119f3eaa5ca2f9627a11
herzo1 <o.herzig95@gmail.com>
2020-09-25 09:05:26 +0200

    used http classes

--- src/main/java/httpserver/core/RequestWorker.java
+++ src/main/java/httpserver/core/RequestWorker.java
@@ -1,5 +1,9 @@
 package httpserver.core;
 
+import httpserver.core.protocol.HttpConstants;
+import httpserver.core.protocol.HttpRequest;
+import httpserver.core.protocol.HttpResponse;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,6 +11,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Logger;
+
+import static httpserver.core.protocol.HttpConstants.*;
 
 /**
  * The class RequestWorker is responsible for the processing of HTTP requests.
@@ -23,9 +29,11 @@ public class RequestWorker implements Runnable {
 
 	public void run() {
 		try (InputStream inputStream = socket.getInputStream(); OutputStream outputStream = socket.getOutputStream()) {
-			Scanner scanner = new Scanner(inputStream);
-			PrintWriter writer = new PrintWriter(outputStream);
-			processRequest(scanner, writer);
+		    HttpRequest request = new HttpRequest(inputStream);
+		    HttpResponse response = new HttpResponse(outputStream);
+		    request.parse();
+		    processRequest(request, response);
+		    response.send();
 		} catch (IOException ex) {
 			LOGGER.severe(ex.toString());
 		} finally {
@@ -33,36 +41,14 @@ public class RequestWorker implements Runnable {
 				socket.close();
 				LOGGER.info("Connection to " + socket.getInetAddress() + " closed");
 			} catch (IOException ex) {
+				LOGGER.warning(ex.toString());
 			}
 		}
 	}
 
-	private void processRequest(Scanner scanner, PrintWriter writer) throws IOException {
-		StringBuilder html = new StringBuilder("<!DOCTYPE html> \n" +
-				"<html>\n" +
-				"<head>\n" +
-				"<title>Request</title>\n" +
-				"</head>\n" +
-				"<body>\n");
-
-
-		// scanner has the http-request present
-		while(scanner.hasNext()) {
-			html.append("<p>");
-			html.append(scanner.nextLine());
-			html.append("</p>\n");
-		}
-
-		html.append("</body>\n" + "</html>");
-
-		writer.println("HTTP/1.1 200 OK"); // status line
-		writer.println("Content-Type: text/html");
-		writer.println("Content-Length: " + html.length());
-		writer.println("Keep-Alive: timeout=5, max=99");
-		writer.println("Connection: Keep-Alive");
-		writer.println(); // empty line
-
-        writer.print(html);
-		writer.flush(); // schickt buffer-inhalt über das netzwerk
+	private void processRequest(HttpRequest request, HttpResponse response) throws IOException {
+	    response.setStatus(STATUS_OK);
+	    response.addHeader(HEADER_CONTENT_TYPE, MIME_TYPE_TEXT_HTML);
+	    response.writeBody("<html><body><h1>Hello World!</h1></body><html>");
 	}
 }
